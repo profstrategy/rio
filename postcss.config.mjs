c5392e14dd88e54d4ad51f55627b7e7bcc9e1827
profstrategy <ismailabdulrahman555@gmail.com>
2026-02-01 11:16:52 +0100

    fix:rename postcss config file to .mjs for esmodule compatitbility

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Changed from 'tailwindcss'
+    '@tailwindcss/postcss': {},
   },
 };
 
